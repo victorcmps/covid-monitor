a845df749cb0ce9aebbd6d399bc9ef6ff4fa9732
jvalmeida <joao.victor_1234@hotmail.com>
2021-04-26 16:52:46 -0300

    (feat): adicionando models

--- src/app/core/services/covid19.service.ts
+++ src/app/core/services/covid19.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Countries } from '../models/countries.model';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +14,8 @@ export class Covid19Service {
     return this.http.get(`https://disease.sh/v3/covid-19/all`);
   }
 
-  getCountriesData(country?: number): Observable<any> {
-    return this.http.get(`https://disease.sh/v3/covid-19/countries/${country || ''}`);
+  getCountriesData(country?: number): Observable<Countries | Countries[]> {
+    return this.http.get<Countries | Countries[]>(`https://disease.sh/v3/covid-19/countries/${country || ''}`);
   }
 
   getHistory(country?: number): Observable<any> {
